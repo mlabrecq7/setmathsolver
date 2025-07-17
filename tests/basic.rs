1e0b19e51c5acd09d8563a1d5389eed4f4475662
mlabrecq7 <mlabrecq7@gmail.com>
2025-07-17 16:11:47 -0400

    learned how to use a struct

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,4 +1,21 @@
+#[derive(PartialEq, Debug)]
+struct TestData {
+       id: u64,
+}
+
 #[test]
 fn test_basic_union() {
-   assert_eq!(1, 1);
+
+   let data1 = TestData {
+       id: 1,
+   };
+   let data2 = TestData {
+       ..data1
+   };
+
+   assert_eq!(data1, data2);
+
+   //this is where I need to actually use some code! I don't know how yet
+
+
 }
